e50c97de9ed204e3adc13431414637f24bcb9ff2
pancongliang <75927592+pancongliang@users.noreply.github.com>
2024-09-21 19:01:15 +0800

    Update buildit.sh

--- buildit.sh
+++ buildit.sh
@@ -6,7 +6,7 @@ else
    wget --no-check-certificate -N https://support.arraynetworks.net/prx/000/http/supportportal.arraynetworks.net/downloads/pkg_9_4_0_505/MP_Linux_1.2.18/$RUNIT
 fi
 
-podman build -t motion-pro-vpn-client2.0 .
+podman build -t motion-pro-vpn-client .
 
 read -p "Remove file $RUNIT (y)? " RESP
 if [ "$RESP" = "y" ]; then
