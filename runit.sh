753aa30e0078a62eef76cbace666b204ca8d9e04
pancongliang <75927592+pancongliang@users.noreply.github.com>
2024-09-21 19:04:46 +0800

    Update runit.sh

--- runit.sh
+++ runit.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-podman run -d --hostname motion-pro-vpn --name VPNcontainer --privileged motion-pro-vpn-client --method $METHOD --host $HOST --user $USER --passwd $PASSWD --loglevel warn
+podman run -d --hostname motion-pro-vpn --name VPNcontainer --privileged motion-pro-vpn-client --method $METHOD --host $HOST --user $USER --passwd $PASSWD -c inf --loglevel warn
